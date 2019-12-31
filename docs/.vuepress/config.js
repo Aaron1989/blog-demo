be93ab69bfd014bbb65211a690f23d3a813db08c
丁琦诗 <dingqsh@yoozoo.com>
2019-12-31 16:31:12 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  base: '/blog-demo/',
+  title: 'blog-demo',
+  description: 'Vuepress blog demo'
+}
+
+module.exports = {
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/Aaron1989/gitbook-tutorial',
+        // 自定义仓库链接文字。
+        repoLabel: 'My GitHub',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+        ],
+	sidebar: [
+      		['/', '首页'],
+      		['/blog/FirstBlog.md', '我的第一篇博客']
+    ]
+    }
+}
+
